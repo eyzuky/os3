dda0f2c3fece4eeb0efd199d6f38c89aa57cd96a
Eyal Silberman <eyal@200apps.co>
2017-05-04 10:28:54 +0300

    second commit

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,34 @@
+//
+//  MapReduceFramework.cpp
+//  OS3
+//
+//  Created by Eyal Silberman on 04/05/2017.
+//  Copyright © 2017 Eyal Silberman. All rights reserved.
+//
+
+#include "MapReduceFramework.h"
+#include "MapReduceClient.h"
+#include <vector>
+using namespace std;
+OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& itemsVec,
+                                    int multiThreadLevel, bool autoDeleteV2K2)
+{
+    
+    
+    
+    
+    
+    OUT_ITEMS_VEC out_items_vec;
+    return out_items_vec;
+}
+
+
+
+void Emit2 (k2Base*, v2Base*)
+{
+    
+}
+void Emit3 (k3Base*, v3Base*)
+{
+    
+}
