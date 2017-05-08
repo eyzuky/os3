69a975517ac6640e51ac6c607f17d182d3c3cf11
Eyal Silberman <eyal@200apps.co>
2017-05-08 22:06:52 +0300

    first seesion together

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -9,12 +9,51 @@
 #include "MapReduceFramework.h"
 #include "MapReduceClient.h"
 #include <vector>
+#include <pthread.h>
+#include <list>
 using namespace std;
+
+//===============================
+//TYPEDEFS
+
+typedef pair<k2Base,v2Base> map_pair;
+typedef list<map_pair> map_pair_list;
+typedef  pair<k2Base, list<v2Base>> shuffled_pair;
+typedef list<shuffled_pair> shuffled_list;
+typedef pair<k3Base, v3Base> reduced_pair;
+typedef list<reduced_pair> reduced_list;
+
+
+//===============================
+//GLOBALS
+
+
+
+
+
+
+//===============================
+
+
+
+
+
+
+
+
+
+
+
+
 OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& itemsVec,
                                     int multiThreadLevel, bool autoDeleteV2K2)
 {
     
     
+    
+    
+    pthread_t shuffle_thread;
+    pthread_t *map_threads = new pthread_t[multiThreadLevel];
     
     
     
