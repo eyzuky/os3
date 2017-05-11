03d1a176b31ea555d093b7d1283a9cf5388c5c68
Tal Rota <tal.rota@cs.huji.ac.il>
2017-05-11 16:02:37 +0300

    the map data is ready

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 typedef pair<k2Base,v2Base> map_pair;
 typedef list<map_pair> map_pair_list;
-typedef  pair<k2Base, list<v2Base>> shuffled_pair;
+typedef pair<k2Base, list<v2Base>> shuffled_pair;
 typedef list<shuffled_pair> shuffled_list;
 typedef pair<k3Base, v3Base> reduced_pair;
 typedef list<reduced_pair> reduced_list;
@@ -39,11 +39,11 @@ class mapDataHandler {
 
     private:
     unsigned int _bulkIndex;
-    const
-
+    const IN_ITEMS_VEC _items;
+    const MapReduceBase& mapReduceBase;
 
     public:
-    dataHandler(IN_ITEMSstst_VEC& items_vec, const MapReduceBase& mapReduceBase){
+    mapDataHandler(IN_ITEMS_VEC &items_vec, const MapReduceBase& mapReduceBase){
                 _bulkIndex = 0;
                 _items = items_vec;
                 _mapReduceBase = mapReduceBase;
@@ -53,7 +53,13 @@ class mapDataHandler {
     unsigned int nextBulkIndex() const { return _bulkIndex;}
     unsigned int proceedToNextBulk() { this->_bulkIndex = BULK + _bulkIndex;}
     unsigned int getSize() { this->_items.size();}
-
+    void applyMap(const k1Base *const keyOne, const v1Base *const valueOne)
+    {
+        this->mapReduceBase.Map(keyOne,valueOne);
+    }
+    IN_ITEM getItem(unsigned int index){
+        return this->_items[index];
+    }
 };
 
 
@@ -61,12 +67,11 @@ class reduceDataHandler {
 
 private:
     unsigned int _bulkIndex;
-    const IN_ITEMS_VEC _items;
+    const shuffled_list _items;
     const MapReduceBase& mapReduceBase;
 
-
 public:
-    dataHandler(IN_ITEMS_VEC &items_vec, const MapReduceBase& mapReduceBase){
+    reduceDataHandler(shuffled_map &items_vec, const MapReduceBase& mapReduceBase){
         _bulkIndex = 0;
         _items = items_vec;
         _mapReduceBase = mapReduceBase;
@@ -75,18 +80,19 @@ public:
 
     unsigned int nextBulkIndex() const { return _bulkIndex;}
     unsigned int proceedToNextBulk() { this->_bulkIndex = BULK + _bulkIndex;}
-
+    unsigned int getSize() { this->_items.size();}
+    void applyMap(const k2Base *const keyOne, const shuffled_list *const _items)
+    {
+        this->mapReduceBase.reduce(keyOne,valueOne);
+    }
+    IN_ITEM getItem(unsigned int index){
+        return this->_items[index];
+    }
 };
 
 void * mapExec(){
 
 }
-
-
-
-
-
-
 
 
 
