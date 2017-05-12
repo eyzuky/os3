7b97fd7a014dbb4a7d34fda6dcecfafad6b9af70
Eyal Silberman <eyal@200apps.co>
2017-05-12 18:18:03 +0300

    more progress

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -12,6 +12,7 @@
 #include <pthread.h>
 #include <list>
 #include "MapReduceLogger.hpp"
+#include <map>
 //===========================
 //DEFINES
 
@@ -31,31 +32,27 @@ typedef pair<k3Base, v3Base> reduced_pair;
 typedef list<reduced_pair> reduced_list;
 typedef pair<pthread_t, map_pair_list> thread_and_list;
 typedef list<thread_and_list> threads_and_their_list;
-
 //===============================
 //GLOBALS AND MUTEX
 pthread_mutex_t map_mutex;
 pthread_mutex_t index_mutex;
-threads_and_their_list thread_map_list;
-
-
-
+map <pthread_t, pthread_mutex_t> thread_mutex_map;
+map <pthread_t, map_pair_list> thread_list_map;
 
 //=============dast class======
-
+//notice that I almost remade this class, there were tons of conceptual errors (e.g trying to set a const not in the initialization list,
+// or wrong names of variables and stuff. see git commits to see the differences
 class mapDataHandler {
 
     private:
     unsigned int _bulkIndex;
     const IN_ITEMS_VEC _items;
     const MapReduceBase& _mapReduceBase;
-
+    
     public:
     mapDataHandler(IN_ITEMS_VEC &items_vec, const MapReduceBase& mapReduceBase): _items(items_vec), _mapReduceBase(mapReduceBase){
                 _bulkIndex = 0;
    }
-
-    unsigned int nextBulkIndex() const { return _bulkIndex;}
     unsigned int proceedToNextBulk()
     {
         this->_bulkIndex = BULK + _bulkIndex;
@@ -102,40 +99,44 @@ public:
     }
 //    IN_ITEM getItem(unsigned int index){
 //        return this->_items[index];
-//    }
+//    }  //TODO - something here doesn't work
 };
 
 
-//void * frameworkInitialization(){
-//    //todo initialize all the global variables
-//}
-//
+void * frameworkInitialization(){
+    //todo initialize all the global variables
+    
+    return nullptr;
+}
+
 void * mapExec(void * data){
 
     return nullptr;
 }
-//
-//
-//void * shuffle(){
-//
-//}
-//
-//
-//void * joinQueues(){
-//
-//}
-//
-//
-//void * reduceExec(){
-//
-//}
+
+void * shuffle(void * data){
+
+    return nullptr;
+}
+
+
+void * joinQueues(void * data){
+
+    return nullptr;
+}
+
+
+void * reduceExec(void * data){
+
+    return nullptr;
+}
 
 
 
 OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& itemsVec,
                                     int multiThreadLevel, bool autoDeleteV2K2)
 {
-   // frameworkInitialization();
+    frameworkInitialization();
     MapReduceLogger *logger = new MapReduceLogger();
     logger->logInitOfFramework(multiThreadLevel);
     logger->startTimeMap();
@@ -144,17 +145,28 @@ OUT_ITEMS_VEC RunMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_VEC& item
     mapDataHandler map_handler = mapDataHandler(itemsVec, mapReduce);
     pthread_t *map_threads = new pthread_t[multiThreadLevel];
     pthread_t shuffle_thread;
+    
     pthread_mutex_lock(&map_mutex);
     //this loop creates a thread, a list for a thread and adds it to our threads-list list.
     for (int i = 0; i < multiThreadLevel; i++)
     {
         pthread_create(&map_threads[i], NULL, mapExec, &map_handler);
         map_pair_list list_for_thread;
-        thread_and_list new_pair = make_pair(map_threads[i], list_for_thread);
-        thread_map_list.push_back(new_pair);
+        thread_list_map[map_threads[i]] = list_for_thread;
+        thread_mutex_map[map_threads[i]] = PTHREAD_MUTEX_INITIALIZER;
     }
+    pthread_mutex_unlock(&map_mutex);
     
-    
+    pthread_create(&shuffle_thread, NULL, shuffle, nullptr);
+    for (int i = 0; i < multiThreadLevel; ++i)
+    {
+        pthread_join(map_threads[i], NULL);
+    }
+    pthread_join(shuffle_thread, NULL);
+    for (auto iter = thread_mutex_map.begin(); iter != thread_mutex_map.end(); ++iter)
+    {
+        pthread_mutex_destroy(&(iter->second));
+    }
     //======================
     logger->endTimeMap();
     logger->logMapAndShuffleTime();
